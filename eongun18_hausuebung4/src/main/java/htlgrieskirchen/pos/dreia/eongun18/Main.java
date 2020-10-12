bf5b4f2be26a8f2ac01d4a435dff964b5e36b2d8
eliasongun <eongun40@gmail.com>
2020-10-12 15:14:23 +0200

    Mainklasse erstellt und Klasse Numbers erstellt

---
+++ eongun18_hausuebung4/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htlgrieskirchen.pos.dreia.eongun18;
+
+/**
+ *
+ * @author eongu
+ */
+public class Main {
+    
+    public static void main(String[] args)
+    {
+    
+    
+    
+    }
+}
---
+++ eongun18_hausuebung4/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Numbers.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htlgrieskirchen.pos.dreia.eongun18;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author eongu
+ */
+public class Numbers {
+    
+    public void einlesen()
+    {
+       ArrayList<String> numbersList = new ArrayList();
+       
+       numbers = File.
+    
+    
+    
+    }
+    
+}
