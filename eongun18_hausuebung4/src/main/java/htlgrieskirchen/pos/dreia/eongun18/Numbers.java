d4629869304b593342e2a0ca3b29a721ddcf92f4
eliasongun <eongun40@gmail.com>
2020-10-12 15:41:02 +0200

    Methode einlesen von der CSV-Datei Numbers

--- eongun18_hausuebung4/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Numbers.java
+++ eongun18_hausuebung4/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Numbers.java
@@ -5,22 +5,35 @@
  */
 package htlgrieskirchen.pos.dreia.eongun18;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author eongu
  */
 public class Numbers {
-    
-    public void einlesen()
-    {
-       ArrayList<String> numbersList = new ArrayList();
-       
-       numbers = File.
-    
-    
-    
+
+    public void einlesen() throws FileNotFoundException {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File("numbers.csv")));
+            String strNumb = br.readLine();
+            while (strNumb != null) {
+
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
